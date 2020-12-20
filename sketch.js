85f684b23cef2b74dbb581bc2703b546ab6df59f
rag-02 <75127428+rag-02@users.noreply.github.com>
2020-12-20 15:00:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+ const Engine = Matter.Engine;
+ const World = Matter.World;
+ const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground;
+
+function setup() {
+  createCanvas(3000,800);
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(600, 600, 1200, 20);
+
+
+}
+
+function draw() {
+  background(180);
+  Engine.update(engine);
+  ground.display();
+  
+}
