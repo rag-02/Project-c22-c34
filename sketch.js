6ede32688a99304e678dcf6cacd3bf743eb028bb
ANCY616 <74260028+ANCY616@users.noreply.github.com>
2020-12-21 20:12:15 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,32 +1,53 @@
  const Engine = Matter.Engine;
  const World = Matter.World;
  const Bodies = Matter.Bodies;
+ const Mouse = Matter.Mouse;
+ const Constraint = Matter.Constraint;
+ const MouseConstraint = Matter.MouseConstraint;
 
 var engine, world;
 var ground;
 var box1;
+var pendulum1;
+var sling1;
+var mConstraint;
+
 function setup() {
   createCanvas(3000,800);
   engine = Engine.create();
   world = engine.world;
 
+  let canvasmouse = Mouse.create(canvas.elt);
+  canvasmouse.pixelRatio = pixelDensity();
+  let options = {
+    mouse: canvasmouse
+  };
+
+  mConstraint = MouseConstraint.create(engine, options);
+  World.add(world, mConstraint);
+
   ground = new Ground(600, 600, 1200, 20);
-  box1 = new Box(600,480,70,70);
-  box2 = new Box(600,400,70,70);
-  box3 = new Box(600,320,70,70);
-  box4 = new Box(600,240,70,70);
-  box5 = new Box(600,160,70,70);
-  box6 = new Box(600,80,70,70);
-  box7 = new Box(800,400,70,70);
-  box8 = new Box(800,320,70,70);
-  box9 = new Box(800,240,70,70);
-  box10 = new Box(800,160,70,70);
-  box11 = new Box(800,80,70,70);
-  box12 = new Box(1000,320,70,70);
-  box13 = new Box(1000,240,70,70);
-  box14 = new Box(1000,160,70,70);
-  box15 = new Box(1000,80,70,70);
+  box1 = new Box(600,480,50,50);
+  box2 = new Box(600,400,50,50);
+  box3 = new Box(600,320,50,50);
+  box4 = new Box(600,240,50,50);
+  box5 = new Box(600,160,50,50);
+  box6 = new Box(600,80,50,50);
+  box7 = new Box(700,400,50,50);
+  box8 = new Box(700,320,50,50);
+  box9 = new Box(700,240,50,50);
+  box10 = new Box(700,160,50,50);
+  box11 = new Box(700,80,50,50);
+  box12 = new Box(800,320,50,50);
+  box13 = new Box(800,240,50,50);
+  box14 = new Box(800,160,50,50);
+  box15 = new Box(800,80,50,50);
+  
+  pendulum1 = new Pendulum(340, 450, "brown");
+  sling1 = new Sling(pendulum1.body, { x: 500, y: 100 });
+
 }
+
 
 function draw() {
   background(180);
@@ -47,4 +68,11 @@ function draw() {
   box13.display();
   box14.display();
   box15.display();
+  pendulum1.display();
+  sling1.display();
+}
+
+
+function mouseDragged() {
+  Matter.Body.setPosition(pendulum1.body, { x: mouseX, y: mouseY });
 }
