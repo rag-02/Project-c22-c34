0533d10ba78d8bc5ff0f17cf5cee12c4578a3f0e
ANCY616 <74260028+ANCY616@users.noreply.github.com>
2020-12-21 20:47:13 +0530

    Add files via upload

--- Box.js
+++ Box.js
@@ -1,9 +1,9 @@
 class Box {
     constructor(x, y, width, height) {
       var options = {
-          
-          'friction':1.0,
-          'density':1.0
+          'restitution':0,
+          'friction':0.005,
+          'density':0.8
       }
       this.body = Bodies.rectangle(x, y, width, height, options);
       this.width = width;
